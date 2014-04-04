b05d069da377d328316767eea99231ac14602916
Russ93 <russ93@fullsail.edu>
2014-04-04 11:09:57 -0400

    Full responsive
    - Moved the anchors around for gutters to be handled by bootstrap
    - Instead of JS doing the hover had CSS do it
    - made a better height process or scalability

--- js/main.js
+++ js/main.js
@@ -1,4 +1,2 @@
 $('document').ready(function(){
-    $('.projects li').mouseenter(function(){$(this).css('border','solid 2px #385E8B')})
-    $('.projects li').mouseleave(function(){$(this).css('border','solid 2px #999')})
 })
